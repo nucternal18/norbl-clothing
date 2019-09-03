7b20e19d55d2567150d218af5ca03cdfdb6ca845
A Usoro <aolausoro@gmail.com>
2019-09-03 23:57:53 +0100

    first commit/added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Homepage from './Homepage';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;

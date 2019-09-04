4c1e1f023804a989c792e573fbd9fb354e966cb6
A Usoro <aolausoro@gmail.com>
2019-09-05 00:21:01 +0100

    Homepage/ Broken homepage into smaller components and styled menu-items components

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Directory from '../../components/directory/directory';
+import './Homepage.scss';
+
+const Homepage = () => {
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>
+    );
+}
+
+export default Homepage

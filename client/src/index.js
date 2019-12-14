910529cbf6b5ee5f305c36d4b7aff3a91c638c69
A Usoro <aolausoro@gmail.com>
2019-12-14 12:59:52 +0000

    updated code to be pwa compliant, added serviceworker, https and updated manifest

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
+import * as serviceWorker from './service-worker';
 
 import './index.css';
 import App from './App';
@@ -18,3 +19,5 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 );
+
+serviceWorker.register();
